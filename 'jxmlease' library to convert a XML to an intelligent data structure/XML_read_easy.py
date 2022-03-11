b4a4b850c87188b68b595396d6ffc3a7aacdf2f0
highwayofbits.com <jaroslaw.wojtow@gmail.com>
2022-03-11 15:58:31 +0000

    Update XML_read_easy.py

--- 'jxmlease' library to convert a XML to an intelligent data structure/XML_read_easy.py
+++ 'jxmlease' library to convert a XML to an intelligent data structure/XML_read_easy.py
@@ -7,7 +7,7 @@ host_user = input("Username: ")
 host_pass = getpass.getpass("Password: ")
 
 with Device(host=host_ip,user=host_user, passwd=host_pass, port=22) as dev:
-    list_of_interfaces = dev.rpc.get_interface_information(terse=True, interface_name='ge-0/0/7')
+    list_of_interfaces = dev.rpc.get_interface_information(terse=True)
     myparser = jxmlease.EtreeParser()
     myxml_converted = myparser(list_of_interfaces)
     print("Printing the converted data as dictionary:")
